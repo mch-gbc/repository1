2a64443e00dfa0cd456e97ed69ff58a3b67e5d27
Malaki Holmes <mholmes15@huskers.unl.edu>
2020-08-15 19:20:20 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Malaki Holmes
+ *        mholmes15@huskers.unl.edu
+ * Date: 2020/8/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Malaki Holmes\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
